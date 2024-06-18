c24407270edda0958a038334a8640ef38c5b4c01
UTTAM KUMAR <133669263+UttamKrDEV17@users.noreply.github.com>
2024-06-18 16:50:45 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'b5860f1d1fmsh441b14fc25e0ba7p18b59bjsn9ba16c010159',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "e3fad7d1d0f69cd481c2976c8e9a94e9";
